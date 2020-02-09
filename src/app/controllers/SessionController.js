d44d5cac80ba728e13b0ab2e5d29345b6631276f
Demontie Costa <demontiecosta17@gmail.com>
2020-02-09 09:59:18 -0300

    Codigos refeitos para fixar na mente

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,51 @@
+//  forma de autenticação de api rest
+import jwt from 'jsonwebtoken';
+// Para as validações de campos(eu coloco o * porque o yup não tem o export default, nele so tem funcoes)
+import * as Yup from 'yup';
+
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+  async store(req, res) {
+    // Validações para obrigatoriedade
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha nas validações' });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Usuário não encontrado!' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Senha incorreta!' });
+    }
+
+    const { id, name } = user;
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      // jwt.sign(o que voce quer utilizar, token que so vc saiba, configurações adiconais)
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
