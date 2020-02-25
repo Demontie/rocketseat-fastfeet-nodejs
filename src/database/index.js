4ed2a28ff8366eb45ee796eb204d4d7d39ac6997
Demontie Costa <demontiecosta17@gmail.com>
2020-02-25 20:43:04 -0300

    correcao da seed e adicionado schemas do mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
@@ -10,12 +11,20 @@ const models = [User, Recipient];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databseConfig);
-
     models.map(model => model.init(this.connection));
+  }
+
+  mongo() {
+    this.mongo = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
   }
 }
 
